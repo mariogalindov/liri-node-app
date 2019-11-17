acdaa2aa311ba796ab9fbad1d4ddaaaf60cd9601
Mario Galindo <mariogalindo89@gmail.com>
2019-11-16 18:31:50 -0600

    added the movie-this functionality

--- liri.js
+++ liri.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // Global variables
-var inquirer = require("inquirer"); //Is this needed? 
 
 var axios = require("axios");
 var moment = require("moment");
+var fs = require("fs")
 
     //Spotify setup with keys
 var Spotify = require("node-spotify-api");
@@ -15,7 +15,8 @@ var spotify = new Spotify(keys.spotify);
 var nodeArgs = process.argv;
 var command = process.argv[2];
 var val = ""; 
-var input = nodeArgs.slice(3)
+// var input = nodeArgs.slice(3).join(" ")
+// console.log(input);
 
 // Loop through all the words in the node argument
 // And do a little for-loop magic to handle the inclusion of the characters that will fill the blank spaces 
@@ -31,7 +32,6 @@ for (var i = 3; i < nodeArgs.length; i++) {
 
   }
 }
-
 
 // concert-this function
 var bandsInTown = function() {
@@ -71,7 +71,6 @@ var bandsInTown = function() {
     });
 }
 
-
 // spotify-this-song function
 var spotifyThis = function() {
     // console.log(val)
@@ -91,7 +90,7 @@ var spotifyThis = function() {
           });
         })
     } else {
-        spotify.search({ type: 'track', query: input}, function(err, data) {
+        spotify.search({ type: 'track', query: "The Sign Ace of Base"}, function(err, data) {
             if (err) {
               return console.log('Error occurred: ' + err);
             }
@@ -104,10 +103,93 @@ var spotifyThis = function() {
             console.log(`Song: ${element.name} \nURL: ${element.preview_url} \nAlbum: ${element.album.name}\n`)
           });
         })
-
     }
-
 };
+
+
+//movie-this function
+var movieThis = function() {
+
+    if (val !== "") {
+      var movie = val;
+      var queryURL = "http://omdbapi.com/?apikey=e264beba&t=" + movie
+      
+      
+      axios.get(queryURL).then(
+      function(response) {
+
+        var movieData = response.data
+
+        console.log(`Title: ${movieData.Title} \nYear: ${movieData.Year} \nIMDB Rating: ${movieData.Ratings[0].Value} \nRotten Tomatoes Rating: ${movieData.Ratings[1].Value} \nCountry: ${movieData.Country} \nLanguage: ${movieData.Language} \nPlot: ${movieData.Plot} \nActors: ${movieData.Actors}`)
+
+          
+      })
+      .catch(function(error) {
+          if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+          } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+          } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          }
+          console.log(error.config);
+      });
+        
+    } else {
+      axios.get("http://omdbapi.com/?apikey=e264beba&t=Mr. Nobody.").then(
+        function(response) {
+  
+          var movieData = response.data
+  
+          console.log(`Title: ${movieData.Title} \nYear: ${movieData.Year} \nIMDB Rating: ${movieData.Ratings[0].Value} \nRotten Tomatoes Rating: ${movieData.Ratings[1].Value} \nCountry: ${movieData.Country} \nLanguage: ${movieData.Language} \nPlot: ${movieData.Plot} \nActors: ${movieData.Actors}`)
+  
+            
+        })
+        .catch(function(error) {
+            if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+            } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+            } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+    }
+}
+
+var doWhatItSays = function() {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    if(error) {
+      console.log(error)
+    } console.log(data)
+
+  })
+
+}
+
 
 //   The goddamn switch 
 
@@ -120,12 +202,11 @@ switch (command){
         spotifyThis();
         break;
     
-    case  "total":
-        total();
+    case  "movie-this":
+        movieThis();
         break;
 
-    case "lotto":
-        lotto();
+    case "do-what-it-says":
+        doWhatItSays();
         break;
-
 }
